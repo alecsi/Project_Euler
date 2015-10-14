5dbc1824619d47a170cde3d47d791953b8eae383
alecsi <alecsi@gmail.com>
2015-10-14 15:24:29 +0100

    adding problem 22, currently unfinished

---
+++ problem022.py
@@ -0,0 +1,16 @@
+def solution(csv_file = 'p022_names.txt')
+    """returns sumproduct of alphabetical value and alphabetical position of a list of names"""
+
+    f = open(csv_file, 'r')
+    g = f.read()
+    g = g.replace('"','')
+    x = g.split(',')
+    x = x.sort()
+    
+
+
+
+def string_to_number(input_string)
+    """converts letters to numbers and then sums over the numbers"""
+
+    
